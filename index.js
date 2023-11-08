7b3fc9cc258f4bd69dbca8b7c58f6ebfe13c3312
prabarai <praba.rai@selise.ch>
2023-11-08 17:51:55 +0600

    added text for reference

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const http = require('http');
+// fs is (File System)
 const fs = require('fs');
+const socketIo = require('socket.io');
 const path = require('path');
 
+const port = 5000;
 const server = http.createServer((req, res) => {
+// checking if its a root url
     if (req.url === '/') {
+        // path the html index page
         fs.readFile(__dirname + '/index.html', (err, data) => {
             if (err) {
+                // shown when there is issue with loading index.html
                 res.writeHead(500);
+                // error 500 is internal server error
                 res.end('Error loading index.html');
             } else {
+                // The 200 OK status code means that the request was successful
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.end(data);
             }
         });
     }
 
+    // Import CSS on NodeJS without Express but some how this code is not working
     const staticDir = path.join(__dirname, 'public');
 
     if (req.url === '/public/style.css') {
@@ -29,10 +38,11 @@ const server = http.createServer((req, res) => {
 
 });
 
-const io = require('socket.io')(server);
-const port = 5000;
+const io = socketIo(server);
 
 io.on('connection', (socket) => {
+    console.log('A user connected');
+
     socket.on('send name', (user) => {
         io.emit('send name', user);
     });
@@ -40,8 +50,12 @@ io.on('connection', (socket) => {
     socket.on('send message', (chat) => {
         io.emit('send message', chat);
     });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
 server.listen(port, () => {
-    console.log(`Server is listening at the port: ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
